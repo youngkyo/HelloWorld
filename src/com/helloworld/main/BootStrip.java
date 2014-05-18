b5f51efbf1db07bb106f61607e8c898bd4952275
youngkyo <ykyo.jung@gmail.com>
2014-05-18 22:19:29 +0900

    add BootStrip File

---
+++ src/com/helloworld/main/BootStrip.java
@@ -0,0 +1,10 @@
+package com.helloworld.main;
+
+public class BootStrip {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello World!");
+	}
+
+}
